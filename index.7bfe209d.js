9d4ae6b8b43aa4e3c1b14a0f8dd683e8e18c4666
Kseniia Chepur <kseniia.chepur@gmail.com>
2024-01-07 18:10:18 +0200

    make build

---
+++ index.7bfe209d.js
@@ -0,0 +1,2 @@
+var r=document.querySelectorAll("tr"),e=!0,l=!1,t=void 0;try{for(var n,o=r[Symbol.iterator]();!(e=(n=o.next()).done);e=!0){var a=n.value;a.insertBefore(a.children[1].cloneNode(!0),a.children[4])}}catch(r){l=!0,t=r}finally{try{e||null==o.return||o.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.7bfe209d.js.map
